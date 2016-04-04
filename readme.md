04ec40dfa40b29f792d8f412c3a8f240091dd031
neutmute <neutmute@users.noreply.github.com>
2016-04-04 12:04:24 +1000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Graph Story Client #
+> A client for API's exposed by [Graph Story](https://www.graphstory.com)
+
---
+++ src/graph-story-client/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("graph_story_client")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("graph_story_client")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
