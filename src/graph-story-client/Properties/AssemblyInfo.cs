9b1fae8a12d90e83332ee9a18cf5b9edee4490e5
neutmute <neutmute@users.noreply.github.com>
2016-05-18 11:25:04 +1000

    TeamCity assembly info stamping compat

--- src/graph-story-client/Properties/AssemblyInfo.cs
+++ src/graph-story-client/Properties/AssemblyInfo.cs
@@ -2,11 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("graph_story_client")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("graph_story_client")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("API library for GraphStory.com")]
 
 [assembly: ComVisible(false)]
+
+#if DEBUG
+    [assembly: AssemblyProduct("Graph Story Client (Debug)")]
+    [assembly: AssemblyConfiguration("Debug")]
+#else
+    [assembly: AssemblyProduct("Graph Story Client (Release)")]
+    [assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("Developer Build")]
